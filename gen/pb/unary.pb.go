0f0b85fbfc71a7ef68a77068dc6748ab1e9b5e96
kiki-ki <malpie0619@gmail.com>
2021-06-03 15:37:02 +0900

    unary

--- gen/pb/unary.pb.go
+++ gen/pb/unary.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // 	protoc-gen-go v1.26.0
 // 	protoc        v3.15.7
-// source: call.proto
+// source: unary.proto
 
 package pb
 
@@ -35,7 +35,7 @@ type CallMeJohnRequest struct {
 func (x *CallMeJohnRequest) Reset() {
 	*x = CallMeJohnRequest{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_call_proto_msgTypes[0]
+		mi := &file_unary_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -48,7 +48,7 @@ func (x *CallMeJohnRequest) String() string {
 func (*CallMeJohnRequest) ProtoMessage() {}
 
 func (x *CallMeJohnRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_call_proto_msgTypes[0]
+	mi := &file_unary_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -61,7 +61,7 @@ func (x *CallMeJohnRequest) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use CallMeJohnRequest.ProtoReflect.Descriptor instead.
 func (*CallMeJohnRequest) Descriptor() ([]byte, []int) {
-	return file_call_proto_rawDescGZIP(), []int{0}
+	return file_unary_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *CallMeJohnRequest) GetName() string {
@@ -82,7 +82,7 @@ type CallMeJohnResponse struct {
 func (x *CallMeJohnResponse) Reset() {
 	*x = CallMeJohnResponse{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_call_proto_msgTypes[1]
+		mi := &file_unary_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -95,7 +95,7 @@ func (x *CallMeJohnResponse) String() string {
 func (*CallMeJohnResponse) ProtoMessage() {}
 
 func (x *CallMeJohnResponse) ProtoReflect() protoreflect.Message {
-	mi := &file_call_proto_msgTypes[1]
+	mi := &file_unary_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -108,7 +108,7 @@ func (x *CallMeJohnResponse) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use CallMeJohnResponse.ProtoReflect.Descriptor instead.
 func (*CallMeJohnResponse) Descriptor() ([]byte, []int) {
-	return file_call_proto_rawDescGZIP(), []int{1}
+	return file_unary_proto_rawDescGZIP(), []int{1}
 }
 
 func (x *CallMeJohnResponse) GetMessage() string {
@@ -118,44 +118,44 @@ func (x *CallMeJohnResponse) GetMessage() string {
 	return ""
 }
 
-var File_call_proto protoreflect.FileDescriptor
+var File_unary_proto protoreflect.FileDescriptor
 
-var file_call_proto_rawDesc = []byte{
-	0x0a, 0x0a, 0x63, 0x61, 0x6c, 0x6c, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x04, 0x63, 0x61,
-	0x6c, 0x6c, 0x22, 0x27, 0x0a, 0x11, 0x43, 0x61, 0x6c, 0x6c, 0x4d, 0x65, 0x4a, 0x6f, 0x68, 0x6e,
-	0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x12, 0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18,
-	0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x22, 0x2e, 0x0a, 0x12, 0x43,
-	0x61, 0x6c, 0x6c, 0x4d, 0x65, 0x4a, 0x6f, 0x68, 0x6e, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73,
-	0x65, 0x12, 0x18, 0x0a, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x18, 0x01, 0x20, 0x01,
-	0x28, 0x09, 0x52, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x32, 0x49, 0x0a, 0x04, 0x43,
-	0x61, 0x6c, 0x6c, 0x12, 0x41, 0x0a, 0x0a, 0x43, 0x61, 0x6c, 0x6c, 0x4d, 0x65, 0x4a, 0x6f, 0x68,
-	0x6e, 0x12, 0x17, 0x2e, 0x63, 0x61, 0x6c, 0x6c, 0x2e, 0x43, 0x61, 0x6c, 0x6c, 0x4d, 0x65, 0x4a,
-	0x6f, 0x68, 0x6e, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x18, 0x2e, 0x63, 0x61, 0x6c,
-	0x6c, 0x2e, 0x43, 0x61, 0x6c, 0x6c, 0x4d, 0x65, 0x4a, 0x6f, 0x68, 0x6e, 0x52, 0x65, 0x73, 0x70,
-	0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x42, 0x08, 0x5a, 0x06, 0x67, 0x65, 0x6e, 0x2f, 0x70, 0x62,
-	0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+var file_unary_proto_rawDesc = []byte{
+	0x0a, 0x0b, 0x75, 0x6e, 0x61, 0x72, 0x79, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x05, 0x75,
+	0x6e, 0x61, 0x72, 0x79, 0x22, 0x27, 0x0a, 0x11, 0x43, 0x61, 0x6c, 0x6c, 0x4d, 0x65, 0x4a, 0x6f,
+	0x68, 0x6e, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x12, 0x0a, 0x04, 0x6e, 0x61, 0x6d,
+	0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x22, 0x2e, 0x0a,
+	0x12, 0x43, 0x61, 0x6c, 0x6c, 0x4d, 0x65, 0x4a, 0x6f, 0x68, 0x6e, 0x52, 0x65, 0x73, 0x70, 0x6f,
+	0x6e, 0x73, 0x65, 0x12, 0x18, 0x0a, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x18, 0x01,
+	0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x32, 0x4b, 0x0a,
+	0x04, 0x43, 0x61, 0x6c, 0x6c, 0x12, 0x43, 0x0a, 0x0a, 0x43, 0x61, 0x6c, 0x6c, 0x4d, 0x65, 0x4a,
+	0x6f, 0x68, 0x6e, 0x12, 0x18, 0x2e, 0x75, 0x6e, 0x61, 0x72, 0x79, 0x2e, 0x43, 0x61, 0x6c, 0x6c,
+	0x4d, 0x65, 0x4a, 0x6f, 0x68, 0x6e, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x19, 0x2e,
+	0x75, 0x6e, 0x61, 0x72, 0x79, 0x2e, 0x43, 0x61, 0x6c, 0x6c, 0x4d, 0x65, 0x4a, 0x6f, 0x68, 0x6e,
+	0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x42, 0x08, 0x5a, 0x06, 0x67, 0x65,
+	0x6e, 0x2f, 0x70, 0x62, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_call_proto_rawDescOnce sync.Once
-	file_call_proto_rawDescData = file_call_proto_rawDesc
+	file_unary_proto_rawDescOnce sync.Once
+	file_unary_proto_rawDescData = file_unary_proto_rawDesc
 )
 
-func file_call_proto_rawDescGZIP() []byte {
-	file_call_proto_rawDescOnce.Do(func() {
-		file_call_proto_rawDescData = protoimpl.X.CompressGZIP(file_call_proto_rawDescData)
+func file_unary_proto_rawDescGZIP() []byte {
+	file_unary_proto_rawDescOnce.Do(func() {
+		file_unary_proto_rawDescData = protoimpl.X.CompressGZIP(file_unary_proto_rawDescData)
 	})
-	return file_call_proto_rawDescData
+	return file_unary_proto_rawDescData
 }
 
-var file_call_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
-var file_call_proto_goTypes = []interface{}{
-	(*CallMeJohnRequest)(nil),  // 0: call.CallMeJohnRequest
-	(*CallMeJohnResponse)(nil), // 1: call.CallMeJohnResponse
+var file_unary_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_unary_proto_goTypes = []interface{}{
+	(*CallMeJohnRequest)(nil),  // 0: unary.CallMeJohnRequest
+	(*CallMeJohnResponse)(nil), // 1: unary.CallMeJohnResponse
 }
-var file_call_proto_depIdxs = []int32{
-	0, // 0: call.Call.CallMeJohn:input_type -> call.CallMeJohnRequest
-	1, // 1: call.Call.CallMeJohn:output_type -> call.CallMeJohnResponse
+var file_unary_proto_depIdxs = []int32{
+	0, // 0: unary.Call.CallMeJohn:input_type -> unary.CallMeJohnRequest
+	1, // 1: unary.Call.CallMeJohn:output_type -> unary.CallMeJohnResponse
 	1, // [1:2] is the sub-list for method output_type
 	0, // [0:1] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -163,13 +163,13 @@ var file_call_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_call_proto_init() }
-func file_call_proto_init() {
-	if File_call_proto != nil {
+func init() { file_unary_proto_init() }
+func file_unary_proto_init() {
+	if File_unary_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_call_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_unary_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*CallMeJohnRequest); i {
 			case 0:
 				return &v.state
@@ -181,7 +181,7 @@ func file_call_proto_init() {
 				return nil
 			}
 		}
-		file_call_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+		file_unary_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*CallMeJohnResponse); i {
 			case 0:
 				return &v.state
@@ -198,20 +198,20 @@ func file_call_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_call_proto_rawDesc,
+			RawDescriptor: file_unary_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
-		GoTypes:           file_call_proto_goTypes,
-		DependencyIndexes: file_call_proto_depIdxs,
-		MessageInfos:      file_call_proto_msgTypes,
+		GoTypes:           file_unary_proto_goTypes,
+		DependencyIndexes: file_unary_proto_depIdxs,
+		MessageInfos:      file_unary_proto_msgTypes,
 	}.Build()
-	File_call_proto = out.File
-	file_call_proto_rawDesc = nil
-	file_call_proto_goTypes = nil
-	file_call_proto_depIdxs = nil
+	File_unary_proto = out.File
+	file_unary_proto_rawDesc = nil
+	file_unary_proto_goTypes = nil
+	file_unary_proto_depIdxs = nil
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -239,7 +239,7 @@ func NewCallClient(cc grpc.ClientConnInterface) CallClient {
 
 func (c *callClient) CallMeJohn(ctx context.Context, in *CallMeJohnRequest, opts ...grpc.CallOption) (*CallMeJohnResponse, error) {
 	out := new(CallMeJohnResponse)
-	err := c.cc.Invoke(ctx, "/call.Call/CallMeJohn", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/unary.Call/CallMeJohn", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -273,7 +273,7 @@ func _Call_CallMeJohn_Handler(srv interface{}, ctx context.Context, dec func(int
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/call.Call/CallMeJohn",
+		FullMethod: "/unary.Call/CallMeJohn",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
 		return srv.(CallServer).CallMeJohn(ctx, req.(*CallMeJohnRequest))
@@ -282,7 +282,7 @@ func _Call_CallMeJohn_Handler(srv interface{}, ctx context.Context, dec func(int
 }
 
 var _Call_serviceDesc = grpc.ServiceDesc{
-	ServiceName: "call.Call",
+	ServiceName: "unary.Call",
 	HandlerType: (*CallServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
@@ -291,5 +291,5 @@ var _Call_serviceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "call.proto",
+	Metadata: "unary.proto",
 }
